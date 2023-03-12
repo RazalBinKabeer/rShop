76fcf235c3852283a5be481978b57323096148bd
razal kabeer <razalbinkabeer01@gmail.com>
2023-03-12 16:24:38 +0400

    commit

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 import os
 from random import randint
-for i in range(300, 400, 3):
-    for j in range(0, randint(1, 10)):
-        d = str(i) + ' days ago'
-        with open('file.txt', 'a') as file:
-            file.write(d)
-        
-        os.system('git add .')
-        os.system('git commit --date="' + d + '" -m "commit')
+# for i in range(300, 400, 3):
+for j in range(30):
+    d = str(319) + ' days ago'
+    with open('file.txt', 'a') as file:
+        file.write(d)
+    
+    os.system('git add .')
+    os.system('git commit --date="' + d + '" -m "commit')
 
 os.system('git push')
+
+319
