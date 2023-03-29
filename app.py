1faa99829cf0148e5d9e31775a429014eac5643d
razal kabeer <razalbinkabeer01@gmail.com>
2023-03-29 16:37:50 +0400

    commit

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from random import randint
 # for i in range(300, 400, 3):
 for j in range(40):
-    d = str(304) + ' days ago'
+    d = str(302) + ' days ago'
     with open('file.txt', 'a') as file:
         file.write(d)
     
